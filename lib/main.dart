eda298865e87be1ea31fefd14dcef1b02d0ae5e5
AARON_BURCIAGA_CORTES <a.21308051280321@cbtis128.edu.mx>
2024-06-03 14:58:19 +0000

    a

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,204 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+        // Set the primary color to dark red
+        primaryColor: Colors.red[900],
+        // Set text theme to light on dark background
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black),
+          bodyText2: TextStyle(color: Colors.black),
+          // Set AppBar text color to white
+          headline6: TextStyle(color: Colors.white),
+        ),
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Login(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(
+          "Pockets",
+          style: TextStyle(color: Colors.white),
+        ),
+        // Set AppBar color to red
+        backgroundColor: Colors.red[900],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0), // Agrega un Padding
+                child: Container(
+                  height: MediaQuery.of(context).size.height *
+                      0.2, // Ajusta la altura de la imagen
+                  child: Image.network(
+                    'https://raw.githubusercontent.com/BurciagaAA128/Img_IOS/main/logodemipockets.png',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Email"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Ingresa tu email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Contraseña"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Ingresa tu contraseña';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        if (emailController.text == "@gmail" &&
+                            passwordController.text == "123456") {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomePage(
+                                      email: emailController.text,
+                                    )),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Credenciales invalidas')),
+                          );
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text('Por favor llena los campos')),
+                        );
+                      }
+                    },
+                    // Set button color to red and text color to white
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red[900],
+                      onPrimary: Colors.white,
+                    ),
+                    child: const Text('Iniciar'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, required this.email}) : super(key: key);
+
+  final String email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Bienvenido a Pockets',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.red[900], // Set AppBar color to red
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Image.network(
+                  'https://raw.githubusercontent.com/BurciagaAA128/Img_IOS/main/logodemipockets.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Text(
+              '¡Bienvenido!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(email),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red[900],
+                  onPrimary: Colors.white,
+                ),
+                child: const Text("Cerrar sesión"),
+              ),
+            ),
+          ],
         ),
       ),
     );
